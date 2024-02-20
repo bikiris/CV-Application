5abab6a94463303d133156e38a911f40f54d574a
bikiris <a664347635@gmail.com>
2024-02-19 20:31:49 -0500

    Remove sections as file + Create at the form

--- src/components/ApplicationForm.jsx
+++ src/components/ApplicationForm.jsx
@@ -1,15 +1,15 @@
-import EducationSection from "./EducationSection";
-import ExperienceSection from "./ExperienceSection";
-import GeneralSection from "./GeneralSection";
+import AddSection from './AddSection';
 import './ApplicationForm.css'; 
 function ApplicationForm () {
     return (
-        <div className="Form">
-            <h1>Application Form</h1>
-            <GeneralSection />
-            <EducationSection />
-            <ExperienceSection />
-        </div>
+        <>
+            <div className="Form">
+                <h1>Application Form</h1>
+                <AddSection sectionName={"General Information"} fields={["name", "email", "phone", "address", "linkedin"]} />
+                <AddSection sectionName={"Education Information"} fields={["School", "Degree", "Graduation Date"]} />
+                <AddSection sectionName={"Experience Information"} fields={["Job Title", "employer", "start Date", "end Date", "location"]} />
+            </div>
+        </>
     )
 }
 
